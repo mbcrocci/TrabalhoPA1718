4d1774379c8a757e847d1d0540a775502dad0110
Maurizio Crocci <mbcrocci@gmail.com>
2018-04-26 17:22:17 +0100

    Initial class creation

---
+++ 9CardSiege/src/pkg9cardsiege/logic/attack/Attack.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg9cardsiege.logic.attack;
+
+/**
+ *
+ * @author m0nk1w1
+ */
+public class Attack {
+    
+}
