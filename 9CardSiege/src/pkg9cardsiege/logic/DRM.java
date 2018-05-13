3334426afa80863d56f7564279b9dcc724923b24
punkc4t <punkc4t@protonmail.com>
2018-05-13 16:42:06 +0100

    forgot 1 class

--- 9CardSiege/src/pkg9cardsiege/logic/DRM.java
+++ 9CardSiege/src/pkg9cardsiege/logic/DRM.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pkg9cardsiege.logic;
 
-/**
- *
- * @author m0nk1w1
- */
-public enum DRM {
+import java.io.Serializable;
+
+
+public enum DRM implements Serializable {
     // drms on actions
     SABOTAGE_ACT,
     MORALE_ACT,
--- 9CardSiege/src/pkg9cardsiege/logic/events/TrebuchetEvent.java
+++ 9CardSiege/src/pkg9cardsiege/logic/events/TrebuchetEvent.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg9cardsiege.logic.events;
 
 import java.util.ArrayList;
 import pkg9cardsiege.logic.Game;
 import pkg9cardsiege.logic.cards.StatusCard;
 
-/**
- *
- * @author m0nk1w1
- */
+
 public class TrebuchetEvent extends Event {
     public TrebuchetEvent() {
         this.name = new String("Trebuchet Attack");
